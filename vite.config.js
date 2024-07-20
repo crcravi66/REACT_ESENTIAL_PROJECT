80435ac5ea712298f3a235376d00cc51a64bab14
arun <crcravi66@gmail.com>
2024-07-20 20:51:29 +0530

    updated project name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-esential-project/'
+  base: '/REACT_ESENTIAL_PROJECT/'
 })
